98982fd32637722baecd89606d597854a3709673
jungster <jungster@franciscrow.com>
2014-11-19 00:43:31 -0600

    Still don't know why I can't stop it scrolling.

--- data/pagescroll.js
+++ data/pagescroll.js
@@ -1,15 +1,15 @@
 self.port.on("count", function(count) {
     var c = count;
-    if (c > 0){
-    var a = parseInt(100*$(document).height()/c);
-    $(document).ready(function(){
-     $('body,html').animate({ scrollTop: $(document).height() }, a , "linear"); 
-    });
-    }
-    if (c <= 0){
-    $(document).ready(function(){
-     $('body,html').stop(); 
-    });
-    } 
+    var a = parseInt(100*$(document).height()/(c+1));
+
+    $(document).ready(function (){
+        if (c < 0){
+            $('body,html').clearQueue(); 
+            console.log("exiting");
+            return;
+        } 
+        $('body,html').stop(true,true); 
+        $('body,html').animate({ scrollTop: $(document).height() }, a , "linear"); 
     console.log(count);
+    });
 });
